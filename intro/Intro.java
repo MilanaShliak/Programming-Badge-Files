d6ec6c0993a42ad9414e5ddcb100ff20e8c04044
BananasAmIRite <jasony1230@gmail.com>
2026-07-24 22:07:12 -0400

    Added intro course

---
+++ intro/Intro.java
@@ -0,0 +1,54 @@
+package intro;
+
+// TODO: make tests for all of these
+public class Intro {
+    /*
+        Welcome to Java! 
+        Please follow the prompts below, and write your code in the allotted sections. 
+        When you are finished, run your code by clicking the triangle on the top-right corner. 
+    */
+    public static void main(String[] args) {
+        // 1. Write a line of code to print your name
+        // ---- BEGIN CODE ----
+
+        // ---- END CODE ----
+
+        // 2. Using variables to define things that might change, write a couple lines
+        //  to print "Hi! My name is [...] and I am [...] years old". Then, on a new line, 
+        // write something about yourself, and include a variable 
+        // whose data type you have not yet used. 
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+
+        // 3.1. Write a piece of code which only prints something if one of your above variables
+        //  is a specific value. 
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+
+        // 3.2. Write a piece of code which only prints something if one of your above variables 
+        // is greater than a specific value. 
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 4. Go over the code you just wrote and re-read it. For any portions that may seem unclear, 
+        // write a comment explaining it. For the rest of the sections, please continue annotating your code
+        // with comments. 
+
+
+
+    }
+}
