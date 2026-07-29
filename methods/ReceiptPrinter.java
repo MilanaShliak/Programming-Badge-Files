24640c6792ba76310db3b3375bc34ad09752ac9d
BananasAmIRite <jasony1230@gmail.com>
2026-07-28 21:34:22 -0400

    added receipt printer module for teaching methods and arrays

---
+++ methods/ReceiptPrinter.java
@@ -0,0 +1,73 @@
+package methods;
+
+public class ReceiptPrinter {
+    /*
+        Welcome! In this module, you will be creating a reusabe receipt printer. 
+
+        Please follow the prompts below, and write your code in the allotted sections. 
+        When you are finished, run your code by clicking the triangle on the top-right corner.
+    */
+    public static void main(String[] args) {
+        // 1. Write code to print a divider, then "RECEIPT", then another divider. Below is an example. 
+        // =====================================
+        // RECEIPT
+        // =====================================
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 2. After the header from above, print a list of items with their prices. For example: 
+        //    Burger        $12.99
+        //    Fries         $3.50
+        //    Drink         $2.00
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+        // 3. Now write code to print a divider, then the final price. Below is an example. 
+        // =====================================
+        // SUBTOTAL        $18.49
+        // TOTAL           $19.78
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+
+
+        // 4. Now write another receipt for a different set of items. 
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+
+
+
+        // 5. Notice how this is difficult to write and read. Surely there's a better way. 
+        //    Begin by replacing your code to print the dividers with a method. 
+
+        // 6. Write a method which takes in a subtotal and "returns" the final price (subtotal * (1+tax rate)). Use this method 
+        //    in your code from part (3). 
+
+        // 7. Now create two arrays: one which stores the item names, and one which stores the prices. Modify part (2) to use these item names
+        //    and prices instead of manually printing them. 
+
+        // 8. Create a method which takes in the array of item names and the array of item prices, and prints the itemized list as in part (2). Replace
+        //    your code in part (2) with a call to this method. Doesn't it look much nicer?
+
+        // 9. Create another method which computes the subtotal from an array of item prices. Make sure to return this subtotal! Then, use it in part (3). 
+
+        // 10. Now, take your newly written code from parts (1) through (3) and put that in a method. Make sure to define the necessary parameters for this method. Then, modify part (4) to use this method. 
+
+    }
+}
