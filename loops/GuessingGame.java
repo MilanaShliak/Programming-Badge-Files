a9e99528eb9b1b03513fdddbbfd8f9ac33ad00a9
BananasAmIRite <jasony1230@gmail.com>
2026-07-28 21:00:11 -0400

    Added loops+math course

---
+++ loops/GuessingGame.java
@@ -0,0 +1,49 @@
+package loops;
+
+public class GuessingGame {
+    /*
+        Welcome! In this module, you will be creating a random number guessing game. 
+
+        Please follow the prompts below, and write your code in the allotted sections. 
+        When you are finished, run your code by clicking the triangle on the top-right corner.
+    */
+    public static void main(String[] args) {
+        // 1. Create a variable containing the secret number.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 2. Create a variable for the player's guess. Give it an initial value by prompting the player for a value 
+        //    between 1 and 100. 
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 3. Write an if statement that tells the player whether their guess is too high or too low.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 4. Put your code inside a while loop so that the game continues while the guess is incorrect.
+        //    Make sure the player can change their guess each time through the loop.
+
+
+        // 6. Print a message when the player wins.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+        // 7. Now make the variable an actual secret by randomly generating the number defined in step 1. 
+    }
+}
---
+++ loops/TipCalculator.java
@@ -0,0 +1,47 @@
+package loops;
+
+public class TipCalculator {
+    /*
+        Welcome! In this module, you will be writing a tip calculator
+
+        Please follow the prompts below, and write your code in the allotted sections. 
+        When you are finished, run your code by clicking the triangle on the top-right corner.
+    */
+    public static void main(String[] args) {
+        // 1. Define variables for the meal cost and tip percentage.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 2. Calculate the amount of the tip.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 3. Calculate the total cost.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 4. Print the meal cost, tip, and total.
+        // ---- BEGIN CODE ----
+
+
+
+        // ---- END CODE ----
+
+
+        // 5. Try changing the values above.
+        // What happens if the meal costs $25.50?
+        // What happens if the tip is 20%?
+    }
+}
